19c989b85b72b2efd42634c9eaf7c05e63476c6c
Paul Tan <pjxtan@gmail.com>
2015-01-04 18:09:48 -0500

    Defined unsharp mask filter, summed area table (integral) functions

--- compvis/filters/linear.py
+++ compvis/filters/linear.py
@@ -1,3 +1,7 @@
+"""
+Linear filters.
+"""
+
 import numpy as np
 import scipy.signal as sig
 
@@ -42,3 +46,55 @@ def corner(img):
                                ( 1, -2,  1)))
 
     return sig.convolve2d(img, kernel, mode='full')
+
+def unsharp_mask(img, img_blur, gamma):
+    # Convert to numpy arrays if necessary
+    if not isinstance(img, np.ndarray): img = np.array(img)
+    if not isinstance(img_blur, np.ndarray): img_blur = np.array(img_blur)
+
+    return img * (1.0 - gamma * img_blur * img)
+
+def summed_area_table(img):
+    """
+    Use summed_area_table_fast() instead. Leaving this for reference.
+    """
+    # Convert to numpy array if necessary
+    if not isinstance(img, np.ndarray): img = np.array(img)
+
+    table = np.zeros(img.shape)
+
+    # Raster scan
+    #---- Do top and left edges
+    table[0, 0] = img[0, 0]
+    for i in range(1, img.shape[0]):
+        table[i, 0] = table[i-1, 0] + img[i, 0]
+    for j in range(1, img.shape[1]):
+        table[0, j] = table[0, j-1] + img[0, j]
+    #---- Do the rest
+    for i in range(1, img.shape[0]):
+        for j in range(1, img.shape[1]):
+            table[i, j] = (table[i-1, j] + table[i, j-1] - table[i-1, j-1]
+                          + img[i, j])
+
+    return table
+
+def summed_area_table_fast(img):
+    # Convert to numpy array if necessary
+    if not isinstance(img, np.ndarray): img = np.array(img)
+
+    return img.cumsum(axis=0).cumsum(axis=1)
+
+def summed_area_in_rect(img, table, corners):
+    # Convert to numpy arrays if necessary
+    if not isinstance(img, np.ndarray): img = np.array(img)
+    if not isinstance(table, np.ndarray): table = np.array(table)
+
+    i0, i1, j0, j1 = corners
+
+    integral = 0
+    for i in range(i0, i1):
+        for j in range(j0, j1):
+            integral += (table[i1, j1] - table[i1, j0-1] - table[i0-1, j1]
+                         + table[i0-1, j0-1])
+
+    return integral

243e5d66e349bc59773142485fa4ac8da05d02fb
Paul Tan <pjxtan@gmail.com>
2015-01-13 18:11:07 -0500

    Defined interpolate function

--- compvis/imgproc/pyramids.py
+++ compvis/imgproc/pyramids.py
@@ -5,20 +5,32 @@ import numpy as np
 import scipy.signal as sig
 from compvis.imgproc.filters import kernels
 
-def downsample(img):
+def interpolate(img):
     """
-    Downsample an image by a factor of 2.
+    Interpolate an image with upsampling rate r=2.
     """
-    img_down = np.zeros((2*img.shape[0], 2*img.shape[1]))
-    img_down[::2, ::2] = img[:, :]
+    img_up = np.zeros((2*img.shape[0], 2*img.shape[1]))
 
-    return sig.convolve2d(img_down, kernels.KERNEL_BILINEAR, 'same')
+    # Upsample
+    img_up[::2, ::2] = img
+
+    # Blur (need to scale by 2 since kernel has unit area)
+    return sig.convolve2d(img_up, 2*kernels.KERNEL_GAUSSIAN, 'same')
+
+def decimate(img):
+    """
+    Decimate an image with downsampling rate r=2.
+    """
+    # Blur
+    img_blur = sig.convolve2d(img_down, kernels.KERNEL_GAUSSIAN, 'same')
+
+    # Downsample
+    return img_blur[::2, ::2]
 
 def gaussian_pyramid(img, levels):
     G = [img, ]
 
     for level in range(levels - 1):
-        G.append(downsample(G[-1]))
+        G.append(decimate(G[-1]))
 
     return G
-

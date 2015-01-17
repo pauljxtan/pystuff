25b889f8928f1b80dc5fc54206e7a4ee518b2bce
Paul Tan <pjxtan@gmail.com>
2015-01-17 17:46:37 -0500

    Modified correlation functions to use even weighting and numpy array operations (a lot faster now)

--- compvis/feature/detectors.py
+++ compvis/feature/detectors.py
@@ -1,42 +1,36 @@
 """
 Feature detectors (Szeliski 4.1.1)
 """
-def wssd(img_0, img_1, u, w):
+import numpy as np
+from compvis.utils import get_patch
+
+def sum_sq_diff(img_0, img_1, u, x, y, x_len, y_len):
     """
-    Finds the weighted summed square difference between two image patches.
+    Finds the summed square difference between two image patches.
+    Uses even weighting across the patch.
 
     Parameters :
         img_0, img_1 : two images being compared
-        u            : displacement vector
-        w            : weighting (window) function
+        u            : displacement vector between the patches
+        x, y         : coordinates of top-left corner of first patch
+        x_len, y_len : width and height of first patch
     """
-    # Check that images and window function are the same size
-    if not (img_0.shape == img_1.shape == w.shape):
-        return ValueError("Images and window function must all be same size")
+    patch_0 = get_patch(img_0, x, y, x_len, y_len)
+    patch_1 = get_patch(img_1, x + u[0], y + u[1], x_len, y_len)
 
-    E = 0.0
-    for x in range(img_0.shape[0]):
-        for y in range(img_0.shape[1]):
-            E += w[x, y] * (img_1[x+u[0], y+u[1]] - img_0[x, y])**2
+    return np.sum((patch_1 - patch_0)**2)
 
-    return E
-
-def autocorr(img, delta_u, w):
+def autocorr(img, delta_u, x, y, x_len, y_len):
     """
-    Computes the auto-correlation function.
+    Computes the auto-correlation function for an image patch with a
+    displacement of delta_u. Uses even weighting across the patch.
+    (Just for reference, since this is sum_sq_diff() with both images being
+    the same.)
 
     Parameters :
-        img     : image
-        delta_u : variation in position
-        w       : weighting (window) function
+        img          : image
+        delta_u      : variation in position
+        x, y         : coordinates of top-left corner of patch
+        x_len, y_len : width and height of patch
     """
-    # Check that image and window function are the same size
-    if not (img.shape == w.shape):
-        return ValueError("Image and window function must be same size")
-
-    E = 0.0
-    for x in range(img.shape[0]):
-        for y in range(img.shape[1]):
-            E += w[x, y] * (img[x+u[0], y+u[1]] - img[x, y])**2
-
-    return E
+    return sum_sq_diff(img, img, delta_u, x, y, x_len, y_len)

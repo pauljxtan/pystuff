c573c3eb65c549e7f7ae031f224d4cbc9d1b2f15
Paul Tan <pjxtan@gmail.com>
2015-01-19 20:24:54 -0500

    Added some documentation

--- compvis/feature/detectors.py
+++ compvis/feature/detectors.py
@@ -65,6 +65,14 @@ def autocorr_surface(img, u_x_range, u_y_range, x, y, x_len, y_len):
 def harris(img, sigma_d=1, sigma_i=2, alpha=0.06, filter_type='gaussian'):
     """
     Returns the Harris interest scores for keypoint detection.
+
+    Parameters :
+        img         : image
+        sigma_d     : width of derivative Gaussian
+        sigma_i     : width of integration Gaussian
+        alpha       : parameter used in Harris-Stephens (1988) score
+        filter_type : 'gaussian' or 'sobel'
+
     (Default values for sigma_d and sigma_i from Szeliski pp. 190)
     (Default value for alpha from Szeliski pp. 189)
     """
@@ -90,22 +98,26 @@ def harris(img, sigma_d=1, sigma_i=2, alpha=0.06, filter_type='gaussian'):
     A_yy = ndi.gaussian_filter(I_yy, sigma_i)
     A_xy = ndi.gaussian_filter(I_xy, sigma_i)
 
-    #kernel = gaussian_kernel(sigma_i)
-    #A_xx = sig.convolve2d(I_xx, kernel, mode='same')
-    #A_yy = sig.convolve2d(I_yy, kernel, mode='same')
-    #A_xy = sig.convolve2d(I_xy, kernel, mode='same')
-
     # Harris scores
     A_det = A_xx * A_yy - A_xy**2
     A_tr = A_xx + A_yy
     
-    # Harris-Stephens 1988
+    # (Harris-Stephens 1988)
     #return A_det - alpha * A_tr**2
     
     # Harmonic mean (Brown-Szeliski-Winder 2005)
     return A_det / A_tr
 
-def get_best_scores(scores, n_points, border=10, r_anms=20):
+def select_scores(scores, n_points, border=10):
+    """
+    Selects the best scores from a given map.
+
+    Parameters :
+        scores   : 2D score array
+        n_points : number of points to select
+        border   : minimum distance from image boundaries
+    """
+
     # Mask out points too close to boundary
     mask = np.zeros(scores.shape)
     mask[border:-border, border:-border] = 1
@@ -116,15 +128,15 @@ def get_best_scores(scores, n_points, border=10, r_anms=20):
     #scores_sorted = scores[coords_sorted]
 
     # Apply ANMS selection (prevent dense clusters)
-    supp_radii = get_suppression_radii(scores)
+    #supp_radii = get_suppression_radii(scores)
     # Sort coordinates by supression radii
-    coords_sorted_supp = np.array(np.unravel_index(np.argsort(supp_radii, axis=None), supp_radii.shape)).T
+    #coords_sorted_supp = np.array(np.unravel_index(np.argsort(supp_radii, axis=None), supp_radii.shape)).T
 
     # Get highest scores
-    #best_coords = coords_sorted_score[-n_points:]
+    best_coords = coords_sorted_score[-n_points:]
 
     # Get scores with highest supression radii
-    best_coords = coords_sorted_supp[-n_points:]
+    #best_coords = coords_sorted_supp[-n_points:]
 
     best_scores = [scores[coord[0], coord[1]] for coord in best_coords]
 

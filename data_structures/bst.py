07bebeb08703784c897762c74a2eaf7d95ce5bd5
Paul Tan <pjxtan@gmail.com>
2016-04-14 13:56:29 -0400

    Initial commit; binary search tree with insertion and pre-/in-/post-order traversal methods

---
+++ data_structures/bst.py
@@ -0,0 +1,119 @@
+class BST(object):
+    """
+    A binary search tree.
+    (Essentially a simple wrapper class to encapsulate the root node.)
+    """
+    def __init__(self, value=None):
+        self._root = _BSTNode(value)
+
+    def insert(self, value):
+        """
+        Inserts a value into the tree.
+        """
+        self._root._insert(value)
+
+    # TODO: can we use decorators here?
+    def traverse_preorder(self, visit_func):
+        """
+        Traverses the tree in pre-order fashion, applying the given function to
+        each node encountered and returning a generator containing the results.
+        """
+        if self._root._value:
+            return self._root._traverse_preorder(visit_func)
+
+    def traverse_inorder(self, visit_func):
+        """
+        Traverses the tree in in-order fashion, applying the given function to
+        each node encountered and returning a generator containing the results.
+        """
+        if self._root._value:
+            return self._root._traverse_inorder(visit_func)
+
+    def traverse_postorder(self, visit_func):
+        """
+        Traverses the tree in post-order fashion, applying the given function
+        to each node encountered and returning a generator containing the
+        results.
+        """
+        if self._root._value:
+            return self._root._traverse_postorder(visit_func)
+
+class _BSTNode(object):
+    """
+    A node in a binary search tree.
+    """
+    def __init__(self, value):
+        self._value = value
+        # Left child
+        self._left = None
+        # Right child
+        self._right = None
+
+    def _insert(self, value):
+        """
+        Inserts a value into the tree rooted by this node.
+        """
+        # If current node has a value, look down the tree
+        if self._value:
+            if value < self._value:
+                # If node exists, recursively insert value on left subtree
+                if self._left:
+                    self._left._insert(value)
+                # Otherwise, create new node and add as left child
+                else:
+                    self._left = _BSTNode(value)
+            elif value > self._value:
+                # If node exists, recursively insert value on right subtree
+                if self._right:
+                    self._right._insert(value)
+                # Otherwise, create new node and add as right child
+                else:
+                    self._right = _BSTNode(value)
+            # Value already exists
+            else:
+                raise ValueError("Value (%d) already in tree" % value)
+        # Otherwise, simply set this node to the value
+        else:
+            self._value = value
+
+    def _traverse_preorder(self, visit_func):
+        """
+        Traverses the tree rooted by this node in pre-order fashion, applying
+        the given function to each node encountered and yielding the result.
+        """
+        if self._value:
+            yield visit_func(self)
+        if self._left:
+            for result in self._left._traverse_preorder(visit_func):
+                yield result
+        if self._right:
+            for result in self._right._traverse_preorder(visit_func):
+                yield result
+
+    def _traverse_inorder(self, visit_func):
+        """
+        Traverses the tree rooted by this node in in-order fashion, applying
+        the given function to each node encountered and yielding the result.
+        """
+        if self._left:
+            for result in self._left._traverse_inorder(visit_func):
+                yield result
+        if self._value:
+            yield visit_func(self)
+        if self._right:
+            for result in self._right._traverse_inorder(visit_func):
+                yield result
+
+    def _traverse_postorder(self, visit_func):
+        """
+        Traverses the tree rooted by this node in post-order fashion, applying
+        the given function to each node encountered and yielding the result.
+        """
+        if self._left:
+            for result in self._left._traverse_postorder(visit_func):
+                yield result
+        if self._right:
+            for result in self._right._traverse_postorder(visit_func):
+                yield result
+        if self._value:
+            yield visit_func(self)

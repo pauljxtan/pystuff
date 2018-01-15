4a38ae57d6a7c725a678f3b6feab29d9ce320e12
Paul Tan <pjxtan@gmail.com>
2018-01-14 20:08:44 -0500

    Improved logic for filling orders

--- pyexchange/exchange.py
+++ pyexchange/exchange.py
@@ -19,36 +19,58 @@ class Exchange(object):
         self.bids.append(Bid(units, price))
         # Sort the bids in decreasing price
         self.bids.sort(key=lambda bid: -bid.price)
-        self.fill_orders()
+        self._fill_asks()
 
     def ask(self, units, price):
         """Places an order to sell a number of units at the given price per unit."""
         self.asks.append(Ask(units, price))
         # Sort the asks in increasing price
         self.asks.sort(key=lambda ask: ask.price)
-        self.fill_orders()
+        self._fill_bids()
 
-    def fill_orders(self):
-        """Fills as many orders as possible. (Orders may be partially filled.)"""
-        # TODO: Does it make sense to always start with bids? (And in descending price?)
+    def _fill_bids(self):
+        """Fills as many bids as possible. (Orders may be partially filled.)"""
         for i, bid in enumerate(self.bids):
             units_filled = 0
-            while units_filled < bid.units:
-                for j, ask in enumerate(self.asks):
-                    if ask.price <= bid.price:
-                        remaining_units = bid.units - units_filled
-                        if ask.units > remaining_units:
-                            self.asks[j].units -= remaining_units
-                            units_filled += remaining_units
-                        else:
-                            units_filled += ask.units
-                            self.asks[j].units = 0
-                        self.transactions.append(Transaction("TODO", "TODO", units_filled, ask.price))
-                        # TODO: in an actual exchange we would also charge/pay the buyer/seller, etc.
-                break
+            for j, ask in enumerate(self.asks):
+                if units_filled == bid.units:
+                    break
+                if ask.price <= bid.price:
+                    remaining_units = bid.units - units_filled
+                    if ask.units > remaining_units:
+                        self.asks[j].units -= remaining_units
+                        units_filled += remaining_units
+                    else:
+                        units_filled += ask.units
+                        self.asks[j].units = 0
+                    self.transactions.append(Transaction("TODO", "TODO", units_filled, ask.price))
+                    # TODO: in an actual exchange we would also charge/pay the buyer/seller, etc.
             self.bids[i].units -= units_filled
 
-        # Remove filled orders
+        self._remove_filled_orders()
+
+    def _fill_asks(self):
+        """Fills as many asks as possible. (Orders may be partially filled.)"""
+        for i, ask in enumerate(self.asks):
+            units_filled = 0
+            for j, bid in enumerate(self.bids):
+                if units_filled == ask.units:
+                    break
+                if bid.price >= ask.price:
+                    remaining_units = ask.units - units_filled
+                    if bid.units > remaining_units:
+                        self.bids[j].units -= remaining_units
+                        units_filled += remaining_units
+                    else:
+                        units_filled += bid.units
+                        self.bids[j].units = 0
+                    self.transactions.append(Transaction("TODO", "TODO", units_filled, bid.price))
+                    # TODO: in an actual exchange we would also charge/pay the buyer/seller, etc.
+            self.asks[i].units -= units_filled
+
+        self._remove_filled_orders()
+
+    def _remove_filled_orders(self):
         self.bids = list(filter(lambda bid: bid.units > 0, self.bids))
         self.asks = list(filter(lambda ask: ask.units > 0, self.asks))
 
